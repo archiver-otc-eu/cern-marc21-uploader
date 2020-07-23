e301206c7d053c7708588a5fcf3be8ec09ca60f4
Jakub Kudzia <jakub.kudzia@outlook.com>
2020-07-23 11:34:19 +0200

    improve help message

--- upload.py
+++ upload.py
@@ -23,67 +23,75 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     description='Register files in the Onedata system')
 
-
 requiredNamed = parser.add_argument_group('required named arguments')
 
 requiredNamed.add_argument(
     '-H', '--host',
     action='store',
-    help='Oneprovider host',
+    help='Oneprovider host.',
     dest='host',
     required=True)
 
 requiredNamed.add_argument(
     '-spi', '--space-id',
     action='store',
-    help='Id of the space in which the file will be registered.',
+    help='Id of the space in which the files will be registered.',
     dest='space_id',
     required=True)
 
 requiredNamed.add_argument(
     '-sti', '--storage-id',
     action='store',
-    help='Id of the storage on which the file is located. Storage must be created as an `imported` storage with path type equal to `canonical`.',
+    help='Id of the storage on which the files are located. Storage must be created as an `imported` storage with path type equal to `canonical`.',
     dest='storage_id',
     required=True)
 
 requiredNamed.add_argument(
     '-t', '--token',
     action='store',
-    help='Onedata access token',
+    help='Onedata access token.',
     dest='token',
     required=True)
 
 requiredNamed.add_argument(
     '-c', '--collection-url',
     action='append',
-    help='Open data collection URL.',
+    help='Open data collection URL. Many collections can be passed (e.g. `-c URL1 -c URL2`).',
     dest='collections',
     required=True)
 
-requiredNamed.add_argument(
+parser.add_argument(
     '-m', '--file-mode',
     action='store',
-    help='POSIX mode with which files will be registered, represented as an octal string',
+    help='POSIX mode with which files will be registered, represented as an octal string.',
     dest='mode',
-    default="664"
+    default="0664"
 )
 
-requiredNamed.add_argument(
+parser.add_argument(
     '-dd', '--disable-auto-detection',
     action='store_true',
-    help='Do not automatically detect file attributes and do not check whether file exists on storage.',
+    help='Flag which disables automatic detection of file attributes and verification whether file exists on storage. '
+         'Passing this flag results in faster registration of files but there is a risk of registering files that '
+         'don\'t exist on storage. Such files will be visible in the space but not accessible.',
     dest='disable_auto_detection',
     default=False
 )
 
 parser.add_argument(
-    '-logging', '--logging-frequency',
+    '-lf', '--logging-frequency',
     action='store',
     type=int,
-    help='Frequency of logging. Log will occur after registering every logging_freq number of files',
+    help='Frequency of logging. Log will occur after registering every logging_freq number of files.',
     dest='logging_freq',
     default=None)
+
+parser.add_argument(
+    '-dv', '--disable-cert-verification',
+    action='store_true',
+    help='Flag which disables verification of SSL certificate.',
+    dest='disable_cert_verification',
+    default=False)
 
 
 REGISTER_FILE_ENDPOINT = "https://{0}/api/v3/oneprovider/data/register"
@@ -116,7 +124,7 @@ def register_file(storage_file_id, size, checksum):
         'autoDetectAttributes': not args.disable_auto_detection
     }
     try:
-        response = requests.post(REGISTER_FILE_ENDPOINT.format(args.host), json=payload, headers=headers)
+        response = requests.post(REGISTER_FILE_ENDPOINT.format(args.host), json=payload, headers=headers, verify=(not args.disable_cert_verification))
         if response.status_code == HTTPStatus.CREATED:
             return True
         else:
@@ -200,5 +208,5 @@ for collection_url in args.collections:
                     if args.logging_freq and total_count % args.logging_freq == 0 and total_count > 0:
                         print("Registered {0} files".format(total_count))
 
-print("Total number of registered files: {0}".format(total_count))
+print("\nTotal registered files count: {0}".format(total_count))
 print("Total size: {0}".format(total_size))
